f52f9ce7fc9d03bc82dca2a891164f11f001dc37
Ben Lynch <bdlynch7@gmail.com>
2023-08-15 17:02:06 -0500

    setup imports

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
 
 function App() {
+  // Get airlineList from redux
+  const airlineList = useSelector(store.airlineList)
+  // talk to redux from react
+  const dispatch = useDispatch();
 
+  
   return (
     <div>
       <h1>Redux Airport</h1>
@@ -13,3 +19,18 @@ function App() {
 }
 
 export default App;
+
+
+// For the initial release we just need to know what airlines have planes at this airport.
+
+// Create a form to add airlines, and a table to show all the airlines that have been added.
+
+// ### Tasks
+
+// - Add Redux to the project. 
+//   - Install the necessary dependencies. (`npm install redux@4 react-redux@7 redux-logger@3`)
+//   - The array of airlines should be stored inside the redux state.
+// - Add a form with one input for an airline name. 
+//   - When the user hits an `Add Airline`, button, add that new airline to the redux state.
+// - Add a table to display all the airlines at this airport.
+// - Create separate React components for both the form & table
--- src/index.js
+++ src/index.js
@@ -2,20 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
-/** TODO: import REDUX **/
+// Imports to get redux up & running
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { logger } from 'redux-logger'
 
 
-/** TODO: Add REDUCERS */
 
+
+// * Reducers *
+// Add airline to the redux state
+const airlineList = (state = [], action) => {
+  console.log('Airline Reducer')
+  if (action.type === 'ADD_AIRLINE') {
+    // Create new array with original values
+    // AND the new value from action.payload
+    console.log(`The airline was : ${action.payload}`)
+    return [ ...state, action.payload ];
+  }
+  // instead of assigning state to a variable, 
+  // we return it's value from a function
+  return state
+}
 
 /** TODO: Create store */
+const storeInstance = createStore(
+  airlineList
+)
 
 
-
-// Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
+// Add Provider to let redux and react talk to each other
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+      <Provider store={storeInstance}>
         <App />
+      </Provider>
     </React.StrictMode>
 );
